bd280a348f69cf0983fb459033f513fa899ca1e9
Felix Bärring <felix_barring@hotmail.com>
2016-11-07 20:55:02 +0100

    Added a class that will be responsible to mangage the rendering, centrelising common functionlaity in one place.

---
+++ librecraft/src/graphics/graphicsManager.cpp
@@ -0,0 +1,11 @@
+
+#include "graphicsManager.h"
+
+namespace graphics {
+
+GraphicsManager::GraphicsManager() {
+
+}
+
+
+} /* namespace graphics */
---
+++ librecraft/src/graphics/graphicsManager.h
@@ -0,0 +1,41 @@
+
+#ifndef SRC_GRAPHICS_GRAPHICSMANAGER_H_
+#define SRC_GRAPHICS_GRAPHICSMANAGER_H_
+
+namespace graphics {
+
+// TODO More detailed description
+/**
+ * \brief A class that is responsible of managing the graphics rendering.
+ *
+ *
+ */
+class GraphicsManager {
+public:
+
+// ########################################################
+// Constructor/Destructor #################################
+// ########################################################
+
+    GraphicsManager();
+
+    virtual ~GraphicsManager();
+
+// ########################################################
+// Member Functions########################################
+// ########################################################
+
+
+    void setSunStrenght(float value);
+
+// ########################################################
+// Implementation #########################################
+// ########################################################
+
+private:
+
+};
+
+} /* namespace graphics */
+
+#endif /* SRC_GRAPHICS_GRAPHICSMANAGER_H_ */
