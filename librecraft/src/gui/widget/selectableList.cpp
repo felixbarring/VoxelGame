0c383ad476b5ed32177808093b62fec076f57d87
Felix Bärring <felix_barring@hotmail.com>
2017-01-15 15:29:41 +0100

    Refactored selectablelist

--- librecraft/src/gui/widget/selectableList.cpp
+++ librecraft/src/gui/widget/selectableList.cpp
@@ -23,40 +23,39 @@ using namespace graphics;
 namespace widget {
 
 SelectableList::SelectableList(int id, int x, int y, int width, int height, function<void(int)> observer,
-        unsigned layer)
+    unsigned layer)
     : AbstractWidget(id, static_cast<double>(x), static_cast<double>(y), width, height), m_layer{layer + 1}
 {
 	this->m_observer = observer;
 
 	auto &res = Resources::getInstance();
 
-	m_sprite.reset(new Sprite{static_cast<double>(x), static_cast<double>(y), layer,
-	    static_cast<double>(width), static_cast<double>(height),
-	    res.getTexture(config::gui_data::button)});
-
+	m_sprite = make_shared<Sprite>(static_cast<double>(x), static_cast<double>(y), layer, static_cast<double>(width),
+	        static_cast<double>(height), res.getTexture(config::gui_data::button));
 }
 
 void SelectableList::addListItem(std::string item) {
+    auto x = m_xCoordinate + 5;
+    auto y = m_yCoordinate + m_height - (5 + 30 * (1 + m_buttons.size()));
+    auto width = m_width - 10;
+    auto height = 30;
+    auto func = [&](int id)
+    {
+        auto button = getButtonWithId(id);
+        if (button->isToggled()) {
+            // If there already is a button toggled, untoggle it.
+            if (m_currentlyToggled)
+                m_currentlyToggled->toggle();
 
-    // TODO WTF IS THIS LOL
-    // TODO Fix so that it does not look completely awful.
-	m_buttons.push_back(shared_ptr<ToggleButton>(
-        new ToggleButton(++idCounter, m_xCoordinate + 5,
-                m_yCoordinate + m_height
-                        - (5 + 30 * (1 + m_buttons.size())),
-                m_width - 10, 30, [&](int id)
-                {
-                    auto button = getButtonWithId(id);
-                    if (button->isToggled()) {
-                        if (m_currentlyToggled)
-                        m_currentlyToggled->toggle();
+            m_currentlyToggled = button;
+        } else {
+            // No button is toggled.
+            m_currentlyToggled.reset();
+        }
+    };
 
-                        m_currentlyToggled = button;
-                    } else {
-                        m_currentlyToggled.reset();
-                    }
-                }, item, m_layer)));
-
+    auto button = make_shared<ToggleButton>(++idCounter, x, y, width, height, func, item, m_layer);
+	m_buttons.push_back(std::move(button));
 }
 
 void SelectableList::deleteListItem(std::string item) {
@@ -74,9 +73,9 @@ std::string SelectableList::getSelectedListItem() {
 }
 
 void SelectableList::reset() {
-	if (m_currentlyToggled && m_currentlyToggled->isToggled()) {
+	if (m_currentlyToggled && m_currentlyToggled->isToggled())
 		m_currentlyToggled->toggle();
-	}
+
 	m_currentlyToggled.reset();
 }
 
