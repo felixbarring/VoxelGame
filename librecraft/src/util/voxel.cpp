fa70d9bbe34f272de309afc0ef73c648b9fcf3db
felixbarring <felix_barring@protonmail.com>
2017-12-14 21:45:53 +0100

    Updated the voxel class with documentation and cpp file. Should fix so that its size is 2 bytes instead of 3 by using 4 bits of one char for each light value.

--- librecraft/src/util/voxel.cpp
+++ librecraft/src/util/voxel.cpp
@@ -1,3 +1,52 @@
 #include "voxel.h"
 
+#include <iostream>
+#include <string>
 
+using std::__cxx11::to_string;
+
+Voxel::Voxel() {
+}
+
+Voxel::Voxel(char id, char sun)
+  : m_id{ id } {
+  setSunLightValue(sun);
+}
+
+Voxel::Voxel(char id, char sun, char other)
+  : m_id{ id } {
+  setSunLightValue(sun);
+  setOtherLightValue(other);
+}
+
+void
+Voxel::setId(char value) {
+  m_id = value;
+}
+
+char
+Voxel::getId() {
+  return m_id;
+}
+
+void
+Voxel::setSunLightValue(char value) {
+  assert(value <= config::graphics_data::directSunlightValue);
+  m_lightValues |= (value & 0x0F);
+}
+
+char
+Voxel::getSunLightValue() {
+  return m_lightValues & 0x0F;
+}
+
+void
+Voxel::setOtherLightValue(char value) {
+  assert(value <= config::graphics_data::directSunlightValue);
+  m_otherLight |= (value & 0x0F);
+}
+
+char
+Voxel::getOtherLightValue() {
+  return m_otherLight;
+}
--- librecraft/src/util/voxel.h
+++ librecraft/src/util/voxel.h
@@ -2,67 +2,85 @@
 #define SRC_UTIL_VOXEL_H_
 
 #include <cassert>
-#include <iostream>
-#include <string>
 
 #include "../config/data.h"
 
 /**
+ * @brief Storage class with all data needed to represent a cube.
  *
+ * Class that stores the data values that are needed for a cube. These values
+ * are ID of the cube and the light values which are used to lightning
+ * computations.
  */
 class Voxel {
 public:
+  /**
+   * @brief Create a default voxel. All values will be zero-initialized.
+   */
+  Voxel();
 
-  Voxel() {
-  }
+  /**
+   * @brief Creates a voxel with the specified id and sunlight. The value for
+   * otherlight will be zero_intialized.
+   *
+   * @param id The id value
+   * @param sun The sunlight value
+   */
+  Voxel(char id, char sun);
 
-  Voxel(char id, char sun)
-    : m_id{ id } {
-    setSunLightValue(sun);
-  }
+  /**
+   * @brief Cretes a voxel with the specified id, sunlight, and otherlight.
+   *
+   * @param id The id value
+   * @param sun The sunlight value
+   * @param other The otherlight value
+   */
+  Voxel(char id, char sun, char other);
 
-  Voxel(char id, char sun, char other)
-    : m_id{ id } {
-    setSunLightValue(sun);
-    setOtherLightValue(other);
-  }
+  /**
+   * @brief Set the value of the id
+   *
+   * @param value The new value for the id
+   */
+  void setId(char value);
 
-  void setId(char value) {
-    m_id = value;
-  }
+  /**
+   *
+   * @return The ig value
+   */
+  char getId();
 
-  char getId() {
-    return m_id;
-  }
+  /**
+   * @brief Set the sunlight value
+   *
+   * @param value The new value for the sunlight
+   */
+  void setSunLightValue(char value);
 
-  void setSunLightValue(char value) {
-    assert(value <= config::graphics_data::directSunlightValue);
-  //    m_lightValues |= (value & sunBits);
-    m_lightValues = value;
-  }
+  /**
+   *
+   * @return The sunlight value
+   */
+  char getSunLightValue();
 
-  char getSunLightValue() {
-    return m_lightValues; // & sunBits;
-  }
+  /**
+   * @brief Set the other light value
+   *
+   * @param value The new value for the otherlight
+   */
+  void setOtherLightValue(char value);
 
-  void setOtherLightValue(char value) {
-    assert(value <= config::graphics_data::directSunlightValue);
-  //    m_lightValues |= (value << 4);
-    m_otherLight = value;
-  }
-
-  char getOtherLightValue() {
-    using std::__cxx11::to_string;
-//    std::cout << "Other light value = " + to_string(m_lightValues >> 4) + "\n";
-//    std::cout << "Other light value anded= " + to_string((m_lightValues >> 4) & sunBits) + "\n";
-    return m_otherLight; //(m_lightValues >> 4) & sunBits;
-  }
+  /**
+   *
+   * @return The otherlight value
+   */
+  char getOtherLightValue();
 
 private:
-  static const char sunBits = 0x0F;
-
   char m_id{};
-  unsigned char m_lightValues{}; // Both sun and other.
+  unsigned char m_lightValues{};
+  // TODO Should only need one light value of 8 bits using bitmasking
+  // and shifting.
   unsigned char m_otherLight{};
 };
 
