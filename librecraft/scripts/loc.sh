6e3f05456529d3a3f4e021e23506fef2ab8a357c
felixbarring <felix_barring@protonmail.com>
2018-02-04 17:02:47 +0100

    Fixed the loc script for counting the lines of code in the project.

--- librecraft/scripts/loc.sh
+++ librecraft/scripts/loc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Used to count lines of code
-find . -name "*.h" -or -name "*.cpp" | grep -v './dependencies' | xargs wc -l
+
+find ../src/ -name "*.h" -or -name "*.cpp" | grep -v './dependencies' | xargs wc -l
